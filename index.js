9baf6df7d7dafaabd9e4e2702c130204c3a3a555
Athena <athenachang17@gmail.com>
2024-10-10 11:05:34 -0400

    finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //write your code here
+// 1
+const reverseString = (str) => {
+    let reversed = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        reversed += str[i];
+    }
+    return reversed;
+};
+reverseString("hello");  //"olleh"
+reverseString("oo")     //"oo"
+reverseString("")       //""
+
+// 2
+const reverseZigZagString = (str) => {
+    let reversed = "";
+    for (i = str.length - 1; i >= 0; i--) {
+        i % 2 === 0 ? reversed += str[i].toUpperCase() : reversed += str[i].toLowerCase();
+    }
+    return reversed;
+};
+console.log(reverseZigZagString("helloo")); //"oOlLeH"
+reverseZigZagString("Fellows")    //"sWoLlEf"
+reverseZigZagString("Code Challenge ")  //"eGnElLaHc EdOc"
